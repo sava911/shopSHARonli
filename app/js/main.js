2757b1667894398834defdb49b619250f313567d
sava911 <pesss929@gmail.com>
2020-11-18 21:24:15 +0300

    content block

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,8 @@ $(function(){
 
     $(".banner-section__slider").slick({
         dots: true,
-        prevArrow: '<button><img class="banner-arrow banner-arrow__prev" src="images/content/arrow-left.svg" alt=""></button>',
-        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/content/arrow-right.svg" alt=""></button>',
+        prevArrow: '<button><img class="banner-arrow banner-arrow__prev" src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/arrow-right.svg" alt=""></button>',
     })
 
   

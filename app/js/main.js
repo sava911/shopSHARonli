9aed3a8a878e96b1720a7d39cc2d2b42aaff5028
sava911 <pesss929@gmail.com>
2020-11-18 18:46:39 +0300

    block slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 $(function(){
 
-
+    $(".banner-section__slider").slick({
+        dots: true,
+        prevArrow: '<button><img class="banner-arrow banner-arrow__prev" src="images/content/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/content/arrow-right.svg" alt=""></button>',
+    })
 
   
 });
